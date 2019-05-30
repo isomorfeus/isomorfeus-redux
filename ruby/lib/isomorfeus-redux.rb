8f7bed236f4a0bab2080171cf4dd02edac6b5afb
Jan Biedermann <jan@kursator.de>
2019-05-30 20:24:33 +0200

    Only support isomorfeus directory layout.

--- ruby/lib/isomorfeus-redux.rb
+++ ruby/lib/isomorfeus-redux.rb
@@ -12,10 +12,7 @@ else
 
   Opal.append_path(__dir__.untaint)
 
-  if Dir.exist?(File.join('app', 'isomorfeus'))
-    # Opal.append_path(File.expand_path(File.join('app', 'isomorfeus', 'components')))
-    Opal.append_path(File.expand_path(File.join('app', 'isomorfeus'))) unless Opal.paths.include?(File.expand_path(File.join('app', 'isomorfeus')))
-  elsif Dir.exist?('isomorfeus')
+  if Dir.exist?('isomorfeus')
     # Opal.append_path(File.expand_path(File.join('isomorfeus', 'components')))
     Opal.append_path(File.expand_path('isomorfeus')) unless Opal.paths.include?(File.expand_path('isomorfeus'))
   end
