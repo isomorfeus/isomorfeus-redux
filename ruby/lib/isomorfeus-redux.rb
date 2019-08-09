03de203bf7f270d624ba63dd4e82f63890a89a48
Jan Biedermann <jan@kursator.de>
2019-08-09 17:50:51 +0200

    Expand_path only once.

--- ruby/lib/isomorfeus-redux.rb
+++ ruby/lib/isomorfeus-redux.rb
@@ -16,8 +16,6 @@ else
 
   Opal.append_path(__dir__.untaint)
 
-  if Dir.exist?('isomorfeus')
-    # Opal.append_path(File.expand_path(File.join('isomorfeus', 'components')))
-    Opal.append_path(File.expand_path('isomorfeus')) unless Opal.paths.include?(File.expand_path('isomorfeus'))
-  end
+  path = File.expand_path('isomorfeus')
+  Opal.append_path(path) unless Opal.paths.include?(path)
 end
