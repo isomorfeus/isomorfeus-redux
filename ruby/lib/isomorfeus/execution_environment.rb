3bc7fe1b605af789d94f59e66c3ce7f8b587f610
Jan Biedermann <jan@kursator.de>
2020-02-22 17:01:28 +0100

    Implement further execution environments.

--- ruby/lib/isomorfeus/execution_environment.rb
+++ ruby/lib/isomorfeus/execution_environment.rb
@@ -3,34 +3,74 @@ module Isomorfeus
     class << self
       attr_accessor :on_browser
       attr_accessor :on_ssr
+      attr_accessor :on_server
+      attr_accessor :on_desktop
+      attr_accessor :on_ios
+      attr_accessor :on_android
+      attr_accessor :on_mobile
+      attr_accessor :on_database
 
       def on_browser?
+        # true if running on browser
         @on_browser
       end
 
       def on_ssr?
+        # true if running in server side rendering in restricted node js vm
         @on_ssr
       end
 
       def on_server?
-        false
+        # true if running on server
+        @on_server
+      end
+
+      def on_desktop?
+        # true if running in electron
+        @on_desktop
+      end
+
+      def on_ios?
+        # true if running in react-native on ios
+        @on_ios
+      end
+
+      def on_android?
+        # true if running in react-native on android
+        @on_android
+      end
+
+      def on_mobile?
+        # true if running in react-native
+        @on_mobile
+      end
+
+      def on_database?
+        # true if running in database context
+        @on_database
       end
     end
 
-    self.on_ssr = `!!((typeof process !== 'undefined') && (typeof process.release !== "undefined") && (process.release.name === 'node'))`
-    self.on_browser = !on_ssr
+    self.on_ssr      = `(typeof process   === 'object' && typeof process.release === 'object' && typeof process.release.name === 'string' && process.release.name === 'node') ? true : false`
+    self.on_desktop  = `(typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.toLowerCase().indexOf(' electron/') > -1) ? true : false`
+    self.on_ios      = `(typeof Platform  === 'object' && typeof Platform.OS === 'string' && Platform.OS.toLowerCase().includes?('ios')) ? true : false`
+    self.on_android  = `(typeof Platform  === 'object' && typeof Platform.OS === 'string' && Platform.OS.toLowerCase().includes?('android')) ? true : false`
+    self.on_mobile   = self.on_ios? || self.on_android?
+    self.on_database = false
+    self.on_browser  = !self.on_ssr? && !self.on_desktop? && !self.on_mobile? && !self.on_database?
+    self.on_server   = false
   else
     class << self
-      def on_browser?
-        false
-      end
-
-      def on_ssr?
-        false
-      end
+      def on_ssr?;      false; end
+      def on_desktop?;  false; end
+      def on_ios?;      false; end
+      def on_android?;  false; end
+      def on_mobile?;   false; end
+      def on_database?; false; end
+      def on_browser?;  false; end
 
       def on_server?
-        true
+        true # so true ...
       end
     end
   end
